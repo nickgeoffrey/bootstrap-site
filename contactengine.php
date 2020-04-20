cb1b0e1763746b64f7955313e2dbfbdd1eaa15f8
Nick Alexander <nicholasgalexander@gmail.com>
2020-04-20 17:36:18 +1200

    new commit

---
+++ contactengine.php
@@ -0,0 +1,40 @@
+#!/usr/local/php70/bin/php-cli -q
+<?php
+
+$EmailFrom = "alexng1@student.op.ac.nz";
+$EmailTo = "alexng1@student.op.ac.nz";
+$Subject = "Contact form from Crescendo Music Project";
+$Name = Trim(stripslashes($_POST['Name'])); 
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contact-error.html\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$Email>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contact-thanks.html\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contact-error.html\">";
+}
+?>
